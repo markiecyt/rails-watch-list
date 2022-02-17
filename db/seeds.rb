e342e43f9f6424b65a3ee623e289cb562b4c1e6a
Markie Choi <markie054@gmail.com>
2022-02-17 23:23:08 +0900

    Added some more style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Movie.create(
-  title: 'Wonder Woman 1984',
-  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
-  poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
-  rating: 6.9
-  )
-Movie.create(
-  title: "The Shawshank Redemption",
-  overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
-  poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
-  rating: 8.7
-  )
-Movie.create(
-  title: "Titanic",
-  overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
-  poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
-  rating: 7.9
-  )
-Movie.create(
-  title: "Ocean's Eight",
-  overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
-  poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
-  rating: 7.0
-  )
+
+require 'open-uri'
+
+bms = Bookmark.all
+bms.each do |b|
+  puts "destroying movie id #{b.id}..."
+  b.destroy
+  puts "destroyed"
+end
+puts "destroyed all bookmarks"
+
+
+puts "Cleaning DB"
+movies = Movie.all
+movies.each do |m|
+  puts "destroying movie id #{m.id}..."
+  m.destroy
+  puts "destroyed"
+end
+puts "destroyed all movies"
+
+lists = List.all
+lists.each do |l|
+  puts "destroying list id #{l.id}..."
+  l.destroy
+  puts "destroyed"
+end
+puts "destroyed all lists"
+
+# Movie.create(
+#   title: 'Wonder Woman 1984',
+#   overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
+#   poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+#   rating: 6.9
+#   )
+# Movie.create(
+#   title: "The Shawshank Redemption",
+#   overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
+#   poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+#   rating: 8.7
+#   )
+# Movie.create(
+#   title: "Titanic",
+#   overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
+#   poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
+#   rating: 7.9
+#   )
+# Movie.create(
+#   title: "Ocean's Eight",
+#   overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
+#   poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
+#   rating: 7.0
+#   )
 
 # require 'open-uri'
 
@@ -36,17 +65,23 @@ Movie.create(
 # List.destroy_all
 
 # the Le Wagon copy of the API
-# url = 'http://tmdb.lewagon.com/movie/top_rated'
-# response = JSON.parse(URI.open(url).read)
+url = 'http://tmdb.lewagon.com/movie/top_rated'
 
-# response['results'].each do |movie_hash|
-#   puts
-#   p movie_hash
-#   create an instance with the hash
-#   Movie.create!(
-#     title: movie_hash['title'],
-#     overview: movie_hash['overview'],
-#     poster_url: 'https://image.tmdb.org/t/p/w500' + movie_hash['poster_path'],
-#     rating: movie['rating']
-#   )
-# end
+puts "creating movies"
+
+puts "connecting to api..."
+response = JSON.parse(URI.open(url).read)
+
+response['results'].each do |movie_hash|
+  puts "reading results..."
+  # create an instance with the hash
+  m = Movie.create!(
+    title: movie_hash['title'],
+    overview: movie_hash['overview'],
+    poster_url: 'https://image.tmdb.org/t/p/w500' + movie_hash['poster_path'],
+    rating: movie_hash['rating']
+  )
+  puts "Created movie #{m.id}" if m.id
+end
+
+puts "Created #{Movie.count} movies"
