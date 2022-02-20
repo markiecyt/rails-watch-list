6b4e0575b19afeb44ca3f2c1d6291f236eabf086
Markie Choi <markie054@gmail.com>
2022-02-20 16:59:42 +0900

    Added movie show with links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'movies/show'
   root 'lists#index'
   resources :lists, only: [ :show, :new, :create ] do
     resources :bookmarks, only: [ :new, :create ]
   end
   resources :bookmarks, only: [ :destroy ]
+  resources :movies, only: [ :show ]
 end
